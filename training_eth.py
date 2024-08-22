fc358a353e22d4dce93af17672e02dd9ebe7e4fb
hcagri <hcagri98@gmail.com>
2024-08-22 11:05:23 +0200

    bug_fix

--- training_eth.py
+++ training_eth.py
@@ -46,7 +46,7 @@ def train_hetero_eth(tr_loader, val_loader, te_loader, tr_inds, val_inds, te_ind
 
             batch.to(device)
             if args.flatten_edges:
-                out = model(batch.x_dict, batch.edge_index_dict, batch.edge_attr_dict, batch.simp_edge_batch_dict, batch.timestamp_dict)
+                out = model(batch.x_dict, batch.edge_index_dict, batch.edge_attr_dict, batch.simp_edge_batch_dict)
             else:
                 out = model(batch.x_dict, batch.edge_index_dict, batch.edge_attr_dict)
 
